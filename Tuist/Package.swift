0bcddedd598b1219963361e0334569c4e62e42e4
Roy <shuwj81@daum.net>
2025-10-22 15:40:03 +0900

    ✨[feat]: 기본 템 픞릿 업데이트

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     name: "MultiModuleTemplate",
     dependencies: [
       .package(url: "http://github.com/pointfreeco/swift-composable-architecture", exact: "1.18.0"),
+      .package(url: "https://github.com/johnpatrickmorgan/TCACoordinators.git", exact: "0.11.1"),
       .package(url: "https://github.com/Roy-wonji/WeaveDI.git", from: "3.3.1")
     ]
 )
